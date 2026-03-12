8cd5b80a3e53fc97326a0f90afa9bd130dd77a22
urlsarahz <sarahzg123@gmail.com>
2026-03-11 21:47:10 -0700

    oauth

--- public/publicServer.py
+++ public/publicServer.py
@@ -1,18 +1,43 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 from pathlib import Path
+import httpx
+
+import boto3
+from botocore.exceptions import ClientError
+
+s3_client = boto3.client("s3")
 
 app = FastAPI(
     title="OAM Uploader Public API"
 )
 
+CLIENT_ID = os.environ.get("OAUTH_CLIENT", "client")
+CLIENT_SECRET = os.environ.get("OAUTH_SECRET", "secret")
+REDIRECT_URI = "https://localhost:8080/api/v1/authenticate"
+AUTHORIZE_URL = "https://www.openstreetmap.org/oauth2/authorize"
+TOKEN_URL = "https://www.openstreetmap.org/oauth2/token"
+
 # Example Methods
-@app.get("/api/v1/test")
-async def TestMethod():
-    """
-    Hello world!
-    """
-    return {"message": "What's up?"}
+@app.get("/api/v1/login")
+async def osmlogin():
+    url = f"{AUTHORIZE_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=openid"
+    return RedirectResponse(url)
+
+@app.get("/api/v1/authenticate")
+async def authenticate(code: str):
+    async with httpx.AsyncClient() as client:
+        payload = {"grant_type": "authorization_code",
+            "code": code,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "redirect_uri": REDIRECT_URI}
+        response = await client.post(TOKEN_URL, data=payload)
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=400)
+    token_data=response.json()
+    return token_data
 
 @app.get("/api/v1/readShared")
 async def TestMethod():
@@ -40,8 +65,7 @@ async def cancel_upload(id: str):
     
     return {
         "status": "success",
-        "message": f"Upload {id} has been successfully aborted.",
-        "dummy_data": True
+        "message": f"Upload {id} has been successfully aborted."
     }
 
 # Serve HTML, keep at bottom
