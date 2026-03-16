44d819dfa7f3719ef456b4509d6f3c03d052e52c
YesRealPerson <spark@confluxes.net>
2026-03-16 13:15:39 -0700

    working uploader demo

--- public/publicServer.py
+++ public/publicServer.py
@@ -2,42 +2,65 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 import botocore.exceptions
 from pathlib import Path
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 import boto3
 from dotenv import load_dotenv
 import os
 from typing import Dict, List
 
-load_dotenv()
+if(not os.getenv("PRODUCTION")):
+    load_dotenv()
+
+S3BUCKET = os.getenv("BUCKET_NAME")
+cors_configuration = {
+    'CORSRules': [
+  {
+    "AllowedOrigins": ["*"],
+    "AllowedMethods": ["GET", "PUT", "POST"],
+    "AllowedHeaders": ["*"],
+    "ExposeHeaders": ["ETag"],
+    "MaxAgeSeconds": 3000
+  }
+]}
 
 # Amazon s3 session
-session = boto3.Session(
-    aws_access_key_id=os.getenv("AWS_PUBLIC_KEY"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
-    region_name=os.getenv("AWS_REGION")
-)
+session = boto3.Session()
 s3 = boto3.client('s3')
 if(len(s3.list_buckets()["Buckets"]) == 0):
     print("Zero buckets!")
-    if(os.getenv("AWS_SECRET_KEY") == "test"):
-        s3.create_bucket(Bucket=os.getenv("BUCKET_NAME"))
+    if(os.getenv("AWS_ACCESS_KEY_ID") == "test"): # If we're in the testing environment we want to setup the bucket and cors policy 
+        s3.create_bucket(Bucket=S3BUCKET)
+        s3.put_bucket_cors(
+            Bucket=S3BUCKET,
+            CORSConfiguration=cors_configuration
+        )
     else:
         raise ValueError("AWS S3 has zero buckets!")
 
 buckets = [x['Name'] for x in s3.list_buckets()['Buckets']]
 
 print("Buckets: ", buckets)
-if(not os.getenv("BUCKET_NAME") in buckets):
-    raise ValueError(os.getenv("BUCKET_NAME")+" does not exist on AWS S3!")
+if(not S3BUCKET in buckets):
+    raise ValueError(S3BUCKET+" does not exist on AWS S3!")
 
+# FastAPI setup
 app = FastAPI(
-    title="OAM Uploader Public API"
+    title="OAM Uploader API Reference",
+    description="""
+[Home](/)
+[Dashboard](/dashboard)
+[Uploader](/upload)
+"""
 )
 
 """
 Notes section:
 TODO: DELETE ME LATER :)
 TODO: Combine repeated body types when all finalized.
+TODO: Add checking for the current user, currently the API is very insecure and will just let users interact with S3, NOT GOOD.
+TODO: Remove raw responses later, currently only for debugging purposes
+TODO: Remove list multipart uploads
+TODO: Specify response schemas when everything is finalized
 """
 
 # Dashboard Endpoints
@@ -88,21 +111,13 @@ async def DeleteEntry(body: DeleteEntryBody):
     }
 
 # S3 Endpoints
-"""
-TODO: Add checking for the current user
-the current user should not have more than 1 (?) upload at a time to prevent abuse, push id to list on create, pop on abort or complete
-TODO: Remove raw responses later, currently only for debugging purposes
-TODO: Remove list multipart uploads
-TODO: Add field validation for createmultipart
-TODO: Specify response schemas
-"""
 @app.get("/api/v1/s3/listmultiparts", tags=["AWS S3"])
 async def listmultipart():
     """
     Temporary endpoint to list s3 multipart uploads
     """
     try:
-        return s3.list_multipart_uploads(Bucket=os.getenv("BUCKET_NAME"))["Uploads"]
+        return s3.list_multipart_uploads(Bucket=S3BUCKET)["Uploads"]
     except KeyError:
         raise HTTPException(status_code=400, detail="No uploads have been started!")
 class createmultipartBody(BaseModel):
@@ -117,7 +132,7 @@ async def createmultipart(body: createmultipartBody):
     userid = "c21782c1-873a-4b79-a3cf-c6a9d25c2e6a" # TODO: Replace me with actual UUID
     key = userid+"-"+body.filename
     response = s3.create_multipart_upload(
-        Bucket=os.getenv("BUCKET_NAME"),
+        Bucket=S3BUCKET,
         Metadata=body.metadata,
         Key=key,
         ContentType=body.contenttype
@@ -135,7 +150,7 @@ class abortmultipartBody(BaseModel):
 async def abortmultipart(body: abortmultipartBody):
     try:
         s3.abort_multipart_upload(
-            Bucket=os.getenv("BUCKET_NAME"),
+            Bucket=S3BUCKET,
             Key=body.key,
             UploadId=body.uploadid
         )
@@ -145,12 +160,8 @@ async def abortmultipart(body: abortmultipartBody):
 
 class partSchema(BaseModel):
     ETag: str
-    ChecksumCRC32: str
-    ChecksumCRC32C: str
-    ChecksumCRC64NVME: str
-    ChecksumSHA1: str
-    ChecksumSHA256: str
     PartNumber: int
+    model_config = ConfigDict(extra='allow')
 class completemultipartBody(BaseModel):
     key: str
     uploadid: str
@@ -158,12 +169,19 @@ class completemultipartBody(BaseModel):
 @app.post("/api/v1/s3/completemultipart", tags=["AWS S3"])
 async def completemultipart(body: completemultipartBody):
     try:
+        parts = [
+            {
+                "ETag": p.ETag,
+                "PartNumber": p.PartNumber
+            }
+            for p in body.parts
+        ]
         s3.complete_multipart_upload(
-            Bucket=os.getenv("BUCKET_NAME"),
+            Bucket=S3BUCKET,
             UploadId=body.uploadid,
             Key=body.key,
-            MulitpartUpload={
-                'Parts': body.parts
+            MultipartUpload={
+                "Parts": parts
             }
         )
     except botocore.exceptions.ClientError as err:
@@ -182,14 +200,16 @@ async def signedurl(body: signedurlBody):
     if(not (0 < body.partnumber <= 10000)):
         raise HTTPException(status_code=400, detail="partnumber must be 1-10,000 (inclusive)!")
     params = {
-        'Bucket': os.getenv("BUCKET_NAME"),
+        'Bucket': S3BUCKET,
         'Key': body.key,
         'PartNumber': body.partnumber,
         'UploadId': body.uploadid
     }
     try:
-        url = s3.generate_presigned_url(ClientMethod="upload_part", Params=params)
-        return url
+        url = s3.generate_presigned_url(ClientMethod="upload_part", Params=params, ExpiresIn=3600)
+        return {
+            'url': url
+        }
     except botocore.exceptions.ClientError as err:
         raise HTTPException(status_code=400, detail=err.response['Error']['Message'])
 
@@ -202,7 +222,7 @@ async def listparts(body: listpartsBody):
     Returns the parts of a multipartupload that have already been uploaded
     """
     try:
-        return s3.list_parts(Bucket=os.getenv("BUCKET_NAME"), Key=body.key, UploadId=body.uploadid)["Parts"]
+        return s3.list_parts(Bucket=S3BUCKET, Key=body.key, UploadId=body.uploadid)["Parts"]
     except botocore.exceptions.ClientError as err:
         raise HTTPException(status_code=400, detail=err.response['Error']['Message'])
     except KeyError as err: # The parts key will not exist if no parts have been uploaded
